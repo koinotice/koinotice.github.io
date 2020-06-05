2982672af51935ae8d2b2bc068dfd9ee0838659a
koinotice <koinotice@gmail.com>
2020-06-05 10:41:42 +0800

    init first commit

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.05b913cc4d7599a3961e32c44f696757.js"
+  "/precache-manifest.db84ebaa273c4a195927e200c84d753c.js"
 );
 
 self.addEventListener('message', (event) => {
